c6543e7f909fdd3efcef33f9aa3bec9caa4564c6
Erik Little <nuclear.ace@gmail.com>
2017-12-09 13:51:31 -0500

    solve day 5

---
+++ Sources/advent/day5.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solveJumpMaze(_ maze: [Int]) -> Int {
+    var maze = maze
+    var ip = 0
+    var steps = 0
+
+    while ip < maze.count {
+        steps += 1
+        (ip, maze[ip]) = (ip + maze[ip], maze[ip] + 1)
+    }
+
+    return steps
+}
