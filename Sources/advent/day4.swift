298374c655080e5563c1d2f00bdcdb9d197849b3
Erik Little <nuclear.ace@gmail.com>
2017-12-09 22:38:27 -0500

    solve day 4 part two

--- Sources/advent/day4.swift
+++ Sources/advent/day4.swift
@@ -2,11 +2,11 @@ import Foundation
 
 // http://adventofcode.com/2017/day/4
 
-func getValidPassphraseCount(_ input: String) -> Int {
+func getValidPassphraseCount(_ input: String, partTwo: Bool = false) -> Int {
     return input.components(separatedBy: "\n")
                 .map({passphrase in
                     let phrases = passphrase.components(separatedBy: " ")
 
-                    return Set(phrases).count == phrases.count ? 1 : 0
+                    return Set(partTwo ? phrases.map({ String($0.sorted()) }) : phrases).count == phrases.count ? 1 : 0
                 }).reduce(0, +)
 }
--- Sources/advent/main.swift
+++ Sources/advent/main.swift
@@ -3,7 +3,7 @@ let partTwo = true
 print("Day 1: ", solveCaptcha(day1Input, partTwo: partTwo))
 print("Day 2: ", checksum(day2Input, partTwo: partTwo))
 print("Day 3: ", getDistance(from: day3Input))
-print("Day 4: ", getValidPassphraseCount(day4Input))
+print("Day 4: ", getValidPassphraseCount(day4Input, partTwo: partTwo))
 print("Day 5: ", solveJumpMaze(day5Input))
 print("Day 6: ", reallocateMemory(day6Input))
 print("Day 7: ", findRootProgram(day7Input))
