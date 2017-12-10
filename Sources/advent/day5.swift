b48e4d7811476ed805e9ba9fcd3c48c6a6c23e03
Erik Little <nuclear.ace@gmail.com>
2017-12-09 22:42:40 -0500

    solve day 5 part 2

--- Sources/advent/day5.swift
+++ Sources/advent/day5.swift
@@ -2,14 +2,14 @@ import Foundation
 
 // http://adventofcode.com/2017/day/5
 
-func solveJumpMaze(_ maze: [Int]) -> Int {
+func solveJumpMaze(_ maze: [Int], partTwo: Bool = false) -> Int {
     var maze = maze
     var ip = 0
     var steps = 0
 
     while ip < maze.count {
         steps += 1
-        (ip, maze[ip]) = (ip + maze[ip], maze[ip] + 1)
+        (ip, maze[ip]) = (ip + maze[ip], partTwo && maze[ip] >= 3 ? maze[ip] - 1 : maze[ip] + 1)
     }
 
     return steps
--- Sources/advent/main.swift
+++ Sources/advent/main.swift
@@ -1,9 +1,7 @@
-let partTwo = true
-
-print("Day 1: ", solveCaptcha(day1Input, partTwo: partTwo))
-print("Day 2: ", checksum(day2Input, partTwo: partTwo))
+print("Day 1: ", solveCaptcha(day1Input))
+print("Day 2: ", checksum(day2Input))
 print("Day 3: ", getDistance(from: day3Input))
-print("Day 4: ", getValidPassphraseCount(day4Input, partTwo: partTwo))
+print("Day 4: ", getValidPassphraseCount(day4Input))
 print("Day 5: ", solveJumpMaze(day5Input))
 print("Day 6: ", reallocateMemory(day6Input))
 print("Day 7: ", findRootProgram(day7Input))
