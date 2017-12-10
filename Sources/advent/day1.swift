2815710dde000972dabf74cd3a49460b8b2c3aa0
Erik Little <nuclear.ace@gmail.com>
2017-12-09 22:20:35 -0500

    solve day 1 part 2

--- Sources/advent/day1.swift
+++ Sources/advent/day1.swift
@@ -1,7 +1,8 @@
 // http://adventofcode.com/2017/day/1
 
-func solveCaptcha(_ input: String) -> Int {
+func solveCaptcha(_ input: String, partTwo: Bool = false) -> Int {
     guard input != "" else { return 0 }
+    guard !partTwo else { return solveCaptchaPartTwo(input) }
 
     let last = Int(String(input.last!))!
     let first = Int(String(input.first!))!
@@ -10,6 +11,30 @@ func solveCaptcha(_ input: String) -> Int {
 
     for i in input.indices where i != input.index(before: input.endIndex) && input[i] == input[input.index(after: i)] {
         sum += Int(String(input[i]))!
+    }
+
+    return sum
+}
+
+func solveCaptchaPartTwo(_ input: String) -> Int {
+    let stepDistance = input.count / 2
+    var sum = 0
+
+    for i in 0..<input.count {
+        let curIndex = input.index(input.startIndex, offsetBy: i)
+        let curNum = input[curIndex]
+        let distanceToEnd = input.distance(from: curIndex, to: input.endIndex)
+        let nextIndex: String.Index
+
+        if stepDistance >= distanceToEnd {
+            nextIndex = input.index(input.startIndex, offsetBy: stepDistance - distanceToEnd)
+        } else {
+            nextIndex = input.index(curIndex, offsetBy: stepDistance)
+        }
+
+        if curNum == input[nextIndex] {
+             sum += Int(String(curNum))!
+        }
     }
 
     return sum
--- Sources/advent/main.swift
+++ Sources/advent/main.swift
@@ -1,4 +1,4 @@
-print("Day 1: ", solveCaptcha(day1Input))
+print("Day 1: ", solveCaptcha(day1Input, partTwo: true))
 print("Day 2: ", checksum(day2Input))
 print("Day 3: ", getDistance(from: day3Input))
 print("Day 4: ", getValidPassphraseCount(day4Input))
